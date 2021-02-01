88281217520a1d8534a8a0b02a18b87955b7be2e
Stephen Neal <stephen.neal14@gmail.com>
2021-02-01 13:48:46 -0500

    MAKE CustomOrderScopeTest with test PeopleOrderedByLastName model

---
+++ tests/CustomOrderScopeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Sfneal\Scopes\Tests;
+
+
+use Sfneal\Scopes\Tests\Models\People;
+use Sfneal\Scopes\Tests\Models\PeopleOrderedByLastName;
+
+class CustomOrderScopeTest extends TestCase
+{
+    /** @test */
+    public function query_is_being_correctly_scoped()
+    {
+        $expected = People::query()
+            ->orderBy('name_last', 'desc')
+            ->get();
+
+        $actual = PeopleOrderedByLastName::query()->get();
+
+        $this->assertEquals($expected->toArray(), $actual->toArray());
+    }
+
+    /** @test */
+    public function query_is_not_being_incorrectly_scoped()
+    {
+        $expected = People::query()
+            ->orderBy('name_last', 'asc')
+            ->get();
+
+        $actual = PeopleOrderedByLastName::query()->get();
+
+        $this->assertNotEquals($expected->toArray(), $actual->toArray());
+    }
+}
---
+++ tests/Models/PeopleOrderedByLastName.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Sfneal\Scopes\Tests\Models;
+
+
+use Sfneal\Scopes\OrderScope;
+
+class PeopleOrderedByLastName extends People
+{
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Global scopes
+        static::addGlobalScope(new OrderScope('name_last'));
+    }
+}
