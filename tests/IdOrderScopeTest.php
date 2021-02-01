a6461bc8953eb9d941d88c6726fa4d13bfbad916
Stephen Neal <stephen.neal14@gmail.com>
2021-02-01 13:45:26 -0500

    MAKE IdOrderScopeTest

---
+++ tests/IdOrderScopeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Sfneal\Scopes\Tests;
+
+
+use Sfneal\Scopes\Tests\Models\People;
+use Sfneal\Scopes\Tests\Models\PeopleOrderedById;
+
+class IdOrderScopeTest extends TestCase
+{
+    /** @test */
+    public function query_is_being_correctly_scoped()
+    {
+        $expected = People::query()
+            ->orderBy('person_id', 'desc')
+            ->get();
+
+        $actual = PeopleOrderedById::query()->get();
+
+        $this->assertEquals($expected->toArray(), $actual->toArray());
+    }
+
+    /** @test */
+    public function query_is_not_being_incorrectly_scoped()
+    {
+        $expected = People::query()
+            ->orderBy('person_id', 'asc')
+            ->get();
+
+        $actual = PeopleOrderedById::query()->get();
+
+        $this->assertNotEquals($expected->toArray(), $actual->toArray());
+    }
+}
